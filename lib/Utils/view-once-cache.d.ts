6e8b010fdd9b61575d7a438c18b68df52b49aa9c
Badzz88 <ibadimufli@gmail.com>
2026-08-14 15:05:51 +0700

    Delete lib/Utils/view-once-cache.d.ts

--- lib/Utils/view-once-cache.d.ts
+++
@@ -1,9 +0,0 @@
-export interface AutoCacheViewOnceOptions {
-	cacheDir?: string
-	logger?: any
-	onCached?: (info: { id: string; jid: string; filePath: string; type: string }) => void
-}
-
-export function getViewOnceContent(message: any): Record<string, any> | null
-
-export function autoCacheViewOnceMedia(sock: any, options?: AutoCacheViewOnceOptions): () => void
